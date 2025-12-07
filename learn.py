0aef4b61862f171775f65582191e249cf3dc7b26
Judthdudeee13 <judthdudeee13@gmail.com>
2025-12-07 09:29:09 -0500

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,122 @@
+#lists
+#lists
+#lists
+#lists
+#lists
+
+print('#lists')
+
+pickewejwiofjo = ['jud','sheepardd','maex','ducklaen','judthdudeaeaeaeaeaeaeaeaehdfeshcujnaouufed']
+print(pickewejwiofjo)
+
+print(pickewejwiofjo[1])
+pickewejwiofjo[1] = 1
+print(pickewejwiofjo[1])
+
+print(pickewejwiofjo[2:4])
+
+pickleweajofjoe = [[1, 2, 3, 4],[1, 2, 3, 4]]
+print(pickleweajofjoe[1])
+print(pickleweajofjoe[1][1])
+
+
+pickleweajofjoe.append(1)
+print(pickleweajofjoe)
+
+del pickleweajofjoe[1]
+
+print(pickleweajofjoe)
+
+
+#tupaware_plastic_is_delicious
+#tupaware_plastic_is_delicious
+#tupaware_plastic_is_delicious
+#tupaware_plastic_is_delicious
+#tupaware_plastic_is_delicious
+#tupaware_plastic_is_delicious
+
+print('#tupaware_plastic_is_delicious')
+
+jude = (1, 2, 3, 4)
+print(jude[1])
+
+
+
+#unchaging variables or constansts
+#unchaging variables or constansts
+#unchaging variables or constansts
+#unchaging variables or constansts
+#unchaging variables or constansts
+print('#unchaging variables or constansts')
+
+jude = 1
+print(jude)
+jude += 2
+print(jude)
+
+JUDE =  1
+print(JUDE)
+JUDE += 2
+print(JUDE)
+
+
+#maps
+#maps
+#maps
+#maps
+#maps
+#maps
+#maps
+
+print('#maps')
+
+map = {'jude': [1, 2, 3, 4, 5, 6, 7, 8, 9], 'shep': 2, 'preston': 3}
+print(map['jude'][8])
+
+# like lists you can append and del from them
+
+
+
+
+#if, elif, else
+#if, elif, else
+#if, elif, else
+#if, elif, else
+#if, elif, else
+#if, elif, else
+#if, elif, else
+#if, elif, else
+#if, elif, else
+#if, elif, else
+
+print('#if, elif, else')
+
+
+age = int(input('How old r u?:'))
+if age <= 10:
+    print ('perfect')
+
+elif age >= 10 and age <= 20:
+    print('your too old')
+
+else:
+    print('gross')
+
+
+
+#loops
+
+#loops
+
+#loops
+
+#loops
+#loops
+#loops
+#loops
+#loops
+#loops
+#loops
+#loops
+
+
